07526f2286cd9022ebcb25dd4ad8d940a6d6a520
lxchuan12-PC\Administrator <lxchuan12@163.com>
2017-02-27 17:56:17 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Content from './Content';
+import InputNumber from './InputNumber';
 
+class Author extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+          author:this.props.author || '轩辕Rowboat'
+        };
+    }
+    handleClick(){
+        console.log('Author handleClick',this);
+    }
+    render () {
+        console.log('Author render',this);
+      return (
+          <span onClick={() => this.handleClick()}>
+              hello {this.state.author}!
+          </span>
+      );
+    }
+};
 class App extends Component {
-  render () {
+  render() {
+      let value = this.props.author;
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Welcome to React</h2>
         </div>
+
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+            To get started, edit <code>src/App.js</code> and save to reload.
+            <br/>
+            <Author author={value}></Author>
         </p>
-        <Content/>
+          <div>
+            <h3>数字输入框</h3>
+            <InputNumber></InputNumber>
+          </div>
       </div>
     );
   }
---
+++ src/InputNumber.js
@@ -0,0 +1,51 @@
+/**
+ * @name: '数字输入框'
+ * @author: 'lxchuan12@163.com'
+ * @time: 2017-02-27
+ */
+import React, {Component, PropTypes} from 'react';
+
+class InputNumber extends Component {
+    constructor() {
+        super();
+        this.state = {
+            amount: 1
+        };
+    }
+
+    changeAmount(ev, num) {
+        if (num === undefined) {
+            this.setState({
+                amount: ev.target.value <= 1 ? 1 : (ev.target.value)
+            });
+        } else {
+            this.setState({
+                amount: (this.state.amount - num) <= 1 ? 1 : (this.state.amount - num)
+            });
+        }
+        console.log(this.state.amount);
+    }
+
+    add(ev) {
+        this.changeAmount(ev, -1);
+    }
+
+    dec(ev) {
+        this.changeAmount(ev, 1);
+    }
+
+    render() {
+        return (
+            <div className="cart-wrap">
+                <input type="text" onChange={(ev) => this.changeAmount(ev)} value={this.state.amount}/>
+                <button onClick={() => this.add()}>+</button>
+                <button onClick={() => this.dec()}>-</button>
+            </div>
+        );
+    }
+}
+;
+InputNumber.propTypes = {
+    amount: React.PropTypes.number
+};
+export  default InputNumber;
