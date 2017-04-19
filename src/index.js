722ce6a3014effda6cdb5423a3ad322cd8f90d2e
lxchuan12 <lxchuan12@163.com>
2017-04-19 23:46:11 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-import React from 'react';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import App from './App';
+// import './index.css';
+
+// ReactDOM.render(
+//   <App author="lxchuan12"/>,
+//   document.getElementById('root')
+// );
+
+// @name:'路由配置'
+// @author:'轩辕Rowboat'
+// @time:'2017.04.19'
+import React,{Component} from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
-
+import {Router,Route,hashHistory} from 'react-router';
+export default class Index extends Component{
+	render(){
+		return (
+			<Router history={hashHistory}>
+				<Route component={App} path="/"></Route>
+			</Router>
+		);
+	};
+}
 ReactDOM.render(
-  <App author="lxchuan12"/>,
+  <Index/>,
   document.getElementById('root')
 );
